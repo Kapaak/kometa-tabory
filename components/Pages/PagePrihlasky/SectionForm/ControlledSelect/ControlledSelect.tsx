52116670a5735bdae09c591b2397b9bd7364f455
kapaak <pavel.zaplet25@gmail.com>
2022-12-05 23:39:26 +0100

    feat: fixing select

--- components/Pages/PagePrihlasky/SectionForm/ControlledSelect/ControlledSelect.tsx
+++ components/Pages/PagePrihlasky/SectionForm/ControlledSelect/ControlledSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useId } from "react";
+import React, { useId, useState } from "react";
 import { Controller } from "react-hook-form";
 import Select from "react-select";
 
@@ -16,26 +16,41 @@ export const ControlledSelect = ({
 	placeholder,
 	options,
 }: ControlledSelectProps) => {
+	const [val, setVal] = useState({ label: "", value: "" });
+
+	const handleChange = (v, callback) => {
+		console.log(v, "val");
+		setVal(v);
+		callback(v);
+	};
 	return (
 		<Controller
 			name={name}
-			render={({ field: { value, onChange, ...restField } }) => (
-				<Select
-					// inputRef={ref}
-					instanceId={useId()}
-					isClearable //asi tky smazat
-					isSearchable
-					placeholder={placeholder}
-					options={options}
-					menuPosition="fixed"
-					menuPlacement="auto"
-					value={options.find(c => c.value === value)}
-					//protoze jinak tam je celej objekt {label,val}
-					onChange={val => onChange(val?.value)}
-					{...restField}
-				/>
-			)}
-			defaultValue=""
+			render={({ field: { onChange, value, name, ...restField } }) => {
+				console.log(value, "xx");
+
+				return (
+					<Select
+						name={name}
+						instanceId={useId()}
+						isClearable
+						isSearchable
+						placeholder={placeholder}
+						options={options}
+						menuPosition="fixed"
+						menuPlacement="auto"
+						// value={value}
+						// value={options.find(c => c.value === value)}
+						//protoze jinak tam je celej objekt {label,val}
+
+						// onChange={val => onChange(val?.value)}
+						value={value}
+						onChange={val => handleChange(val, onChange)}
+						{...restField}
+					/>
+				);
+			}}
+			// defaultValue={{ label: "", value: "" }}
 		/>
 	);
 };
