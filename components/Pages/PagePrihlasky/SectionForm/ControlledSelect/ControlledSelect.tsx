afb5349b50908db35a9178321487bbe026d0b3a9
kapaak <pavel.zaplet25@gmail.com>
2022-12-06 12:37:56 +0100

    feat: updated styles for select

--- components/Pages/PagePrihlasky/SectionForm/ControlledSelect/ControlledSelect.tsx
+++ components/Pages/PagePrihlasky/SectionForm/ControlledSelect/ControlledSelect.tsx
@@ -1,6 +1,11 @@
+import { CaretCircleDown } from "phosphor-react";
 import React, { useId } from "react";
 import { Controller } from "react-hook-form";
-import Select from "react-select";
+import Select, {
+	StylesConfig,
+	components,
+	DropdownIndicatorProps,
+} from "react-select";
 
 type Option = {
 	label: string;
@@ -17,6 +22,28 @@ export const ControlledSelect = ({
 	placeholder,
 	options,
 }: ControlledSelectProps) => {
+	const styles: StylesConfig = {
+		control: (baseStyles, state) => ({
+			// ...baseStyles,
+			display: "flex",
+			flexDirection: "row-reverse",
+			borderBottom: "1px solid var(--collg)",
+			fontFamily: "var(--font2)",
+		}),
+		placeholder: defaultStyles => ({
+			...defaultStyles,
+			fontSize: "1.3rem",
+		}),
+	};
+
+	const DropdownIndicator = (props: DropdownIndicatorProps) => {
+		return (
+			<components.DropdownIndicator {...props}>
+				<CaretCircleDown size={22} cursor="pointer" />
+			</components.DropdownIndicator>
+		);
+	};
+
 	return (
 		<Controller
 			name={name}
@@ -24,7 +51,6 @@ export const ControlledSelect = ({
 				<Select
 					name={name}
 					instanceId={useId()}
-					isClearable
 					isSearchable
 					placeholder={placeholder}
 					options={options}
@@ -32,6 +58,11 @@ export const ControlledSelect = ({
 					menuPlacement="auto"
 					value={value}
 					onChange={val => onChange(val)}
+					styles={styles}
+					components={{
+						DropdownIndicator,
+						IndicatorSeparator: null,
+					}}
 					{...restField}
 				/>
 			)}
