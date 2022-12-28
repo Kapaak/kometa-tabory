8f7b475edf4930bb92a0b5884a4fa14ef2de254d
kapaak <pavel.zaplet25@gmail.com>
2022-12-28 22:16:02 +0100

    feat: add anal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,23 @@ import { GlobalStyles } from "../styles/GlobalStyles";
 import { Header, Footer, HeadSelector } from "@/components";
 import { store } from "@/state";
 import { Provider } from "react-redux";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
+			<Script
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+				strategy="afterInteractive"
+			/>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `}
+			</Script>
 			<HeadSelector />
 			<Header />
 			<GlobalStyles />
