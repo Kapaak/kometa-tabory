ca01186f9277adcc96edf5a4d0ddfbbb61c5ada1
kapaak <pavel.zaplet25@gmail.com>
2022-12-28 21:36:09 +0100

    feat: add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "../styles/GlobalStyles";
-import { Header, Footer } from "@/components";
+import { Header, Footer, HeadSelector } from "@/components";
 import { store } from "@/state";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
+			<HeadSelector />
 			<Header />
 			<GlobalStyles />
 			<Component {...pageProps} />
