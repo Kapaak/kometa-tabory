6ab0617f5565cf12b889dbdd2acefd89a0ef2c00
kapaak <pavel.zaplet25@gmail.com>
2022-12-29 16:41:15 +0100

    feat: change styles

--- components/Shared/IconButton/IconButton.style.ts
+++ components/Shared/IconButton/IconButton.style.ts
@@ -1,4 +1,9 @@
 import { Button as SButton } from "../Button/Button.style";
 import styled from "styled-components";
+import { dimensions } from "utils/breakpoints";
 
-export const Button = styled(SButton)``;
+export const Button = styled(SButton)`
+	@media (${dimensions.tablet}) {
+		margin-left: auto;
+	}
+`;
