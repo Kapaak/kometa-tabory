f9cddae8a092c27e273674887f3f5a7a05cb5ccb
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-01-07 21:08:06 +0100

    feat: add meta description

---
+++ components/Shared/HeadSelector/HeadSelector.data.ts
@@ -0,0 +1,66 @@
+const keywords = `příměstský tábor brno,
+příměstský tábor,
+letní tábory 2023,
+tábory 2023,
+tábory,
+letní tábor,
+dětský tábor,
+příměstské tábory,
+tábor 2023,
+detsky tabor,
+příměstské tábory brno,
+příměstský tábor brno 2023,
+tábor brno,
+tábor pro děti,
+letni tabor,
+primestsky tabor,
+tabor brno,
+primestsky tabor brno,
+letní dětský tábor,
+hry pro děti na tábor,
+celotáborová hra,
+letní příměstské tábory brno 2023,
+letní hry pro děti,
+příměstské tábory brno 2023,
+letní tábory od 12 let,
+stan tábory,
+letní tábory na týden,
+sportovní tábor,
+tábory brno,
+primestske tabory brno,
+příměstský tábor říčany,
+plavecký tábor,
+tabor pro deti,
+co s sebou na tábor,
+plavecký příměstský tábor brno,
+tábory 2023,
+tabory brno,
+plavání kometa,
+aktivity na tábor,
+hry na tábor pro děti,
+letní tábory 2023,
+letní tábor pro děti od 4 let,
+dětský letní tábor
+`;
+
+const mainDescription =
+  "Příměstské tábory pro děti od 4 let do 15 let s lekcemi plavání od licencovaných trenérů s několikaletou praxí. Ve středu je speciální celodenní výlet dle tématu táboru.";
+
+export const data = {
+  "/prihlasky": {
+    // title: "Přihlášky - ",
+    keywords,
+    description:
+      "Nabízíme plavecké kurzy pro děti, školy a školky i dospělé. Učíme plavat zábavnou formou dle nejnovějších metodik.",
+  },
+  "/": {
+    // title: "",
+    keywords,
+    description: mainDescription,
+  },
+  default: {
+    // title: "",
+    keywords,
+    description: mainDescription,
+  },
+};
---
+++ components/Shared/HeadSelector/HeadSelector.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import { data } from "./HeadSelector.data";
+
+type RouteType = "/" | "/prihlasky";
+
+export const HeadSelector = () => {
+  const router = useRouter();
+
+  const currentRoute = router.asPath;
+
+  const currentRouteMetaDesc =
+    data[currentRoute as RouteType]?.description ?? data.default.description;
+
+  return (
+    <Head>
+      <title> Příměšťák Brno | Kometa </title>
+      <link rel="icon" href="/icons/logo-tabory.svg" />
+      <meta name="author" content="Pavel Zapletal & Barbora Novakova" />
+      <meta name="keywords" content={currentRouteMetaDesc} />
+      <link
+        rel="canonical"
+        href={`https://www.plavani-luzanky.cz${currentRoute}`}
+      />
+      <meta
+        name="google-site-verification"
+        content="KV7BmFAqyUFzPz1ft0TFad03jyyUR8o0b-1CvI4FmGs"
+      />
+      <meta name="description" content={currentRouteMetaDesc} />
+      <meta
+        name="keywords"
+        content="plavání Lužánky, bazén Lužánky, bazén za Lužánkami, výuka plavání v Brně, plavání Brno, plavecké kurzy brno, plavání brno, plavání pro děti, plavání s dětmi"
+      />
+    </Head>
+  );
+};
+
+export default HeadSelector;
