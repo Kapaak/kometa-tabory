07fb0c0e4b46c299d65cbe35e940f80c5a9d5eaa
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-04-07 21:12:49 +0200

    feat: add letterspacing to headline

--- styles/UI/MainHeadline.ts
+++ styles/UI/MainHeadline.ts
@@ -6,8 +6,9 @@ export const MainHeadline = styled.h1`
   font-family: var(--font1);
   font-weight: 500;
   font-size: var(--fmheadline);
+  letter-spacing: 0.35rem;
+
   @media (${dimensions.desktop}) {
     line-height: 1;
-    letter-spacing: 0.35rem;
   }
 `;
