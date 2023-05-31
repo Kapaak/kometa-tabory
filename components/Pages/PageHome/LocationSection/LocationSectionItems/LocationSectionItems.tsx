493f4c1b5807d31435d25a5d41712a951a497125
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-05-31 23:18:38 +0200

    feat: update text

--- components/Pages/PageHome/LocationSection/LocationSectionItems/LocationSectionItems.tsx
+++ components/Pages/PageHome/LocationSection/LocationSectionItems/LocationSectionItems.tsx
@@ -18,6 +18,21 @@ const LocationSectionItems = ({ data }: Props) => {
       <S.Text variant="dark" bold style={{ textTransform: "uppercase" }}>
         Bauerova 321/5, Brno 603 00
       </S.Text>
+      <S.Text>
+        Z důvodu opravy je ulice Bauerova zavřena. Místo toho lze použít
+        Expoparking.
+      </S.Text>
+      <S.Text variant="dark" bold>
+        Parkovací dům{" "}
+        <a
+          href="https://www.bvv.cz/navstevnici/doprava/expoparking/"
+          style={{ textDecoration: "underline" }}
+        >
+          Expoparking
+        </a>{" "}
+        a odstavné plochy najdete v blízkosti brány 4 na BVV – některé patří do
+        zóny C (přes den zdarma).
+      </S.Text>
       <S.Text variant="dark">
         Náš tábor se nachází v rozlehlém areálu SKP KOMETA BRNO, který se
         nachází vedle Brněnské Riviéry. Máme tu obrovské travnaté plochy, kde
