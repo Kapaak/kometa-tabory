fc2a3d1c7212e766a1b42aff51e808c9667fa130
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-10-21 17:20:33 +0200

    fix: 500 error

--- next.config.js
+++ next.config.js
@@ -1,26 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	compiler: {
-		styledComponents: true,
-	},
-	webpack: (config, { isServer }) => {
-		if (!isServer) {
-			config.resolve.fallback.fs = false;
-			config.resolve.fallback.tls = false;
-			config.resolve.fallback.net = false;
-			config.resolve.fallback.child_process = false;
-		}
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.child_process = false;
+    }
 
-		return config;
-	},
-	env: {
-		NEXT_PUBLIC_CLIENT_EMAIL: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
-		NEXT_PUBLIC_PRIVATE_KEY: process.env.NEXT_PUBLIC_PRIVATE_KEY,
-		NEXT_PUBLIC_SENDGRID_API_KEY: process.env.NEXT_PUBLIC_SENDGRID_API_KEY,
-		NEXT_PUBLIC_GOOGLE_ADS: process.env.NEXT_PUBLIC_GOOGLE_ADS,
-	},
+    return config;
+  },
+  modularizeImports: {
+    '@phosphor-icons/react': {
+      transform: '@phosphor-icons/react/dist/icons/{{member}}',
+    },
+  },
 };
 
 module.exports = nextConfig;
