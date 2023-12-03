3ba5dd8a234666b559e73115a7033c778c04c653
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-12-03 21:04:11 +0100

    feat: change google analytics library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
 import Script from 'next/script';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 import { Analytics } from '@vercel/analytics/react';
 
@@ -9,30 +9,16 @@ import { PageContextProvider } from '~/contexts/PageContext';
 import { GlobalStyles } from '~/ui/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const isPrihlaskyPage = router.pathname === '/prihlasky/[taborId]';
-
   return (
     <PageContextProvider>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-							gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS}');
-
-							${
-                isPrihlaskyPage
-                  ? `gtag('event', 'conversion', {'send_to': '${process.env.NEXT_PUBLIC_GOOGLE_ADS}/R7_GCPrj24oYEPmetKYp'}`
-                  : ''
-              })
-            `}
-      </Script>
+      <GoogleAnalytics
+        trackPageViews
+        gaMeasurementId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}
+      />
       <Analytics />
       <HeadComponent />
       <GlobalStyles />
